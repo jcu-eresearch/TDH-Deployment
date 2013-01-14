2309d2789884ff6a054abd7a5745b1593ecaa919
NigelB <nigel.blair@gmail.com>
2013-01-15 09:58:53 +1000

    Updating deployment scripts

---
+++ scripts/redbox-deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+SETTINGS_DIR="~/.tdh-deploy-settings"
+SETTINGS_NAME=redbox-settings.sh
+SETTINGS=$SETTINGS_DIR/$SETTINGS_NAME
+
+SCRIPT_DIR=`$(cd  $(dirname $0) ; pwd -P)`
+
+if [ -e "$SETTINGS" ]
+then
+    mkdir -p $SETTINGS_DIR
+    cp $SCRIPT_DIR/$SETTINGS_NAME $SETTINGS
+fi
+
+source $SETTINGS
+
+echo "Project Directory: $PROJECT_HOME"
+
---
+++ scripts/redbox-settings.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+PROJECT_HOME=""
+echo "You need to configure the variables in ~/.tdh-deploy-settings/redbox-settings.sh"; exit 1
+#comment out this ^ line when you have configured the variables
