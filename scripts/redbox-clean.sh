052386d2e16e52ceb478015777b2f43c75bcfd34
NigelB <nigel.blair@gmail.com>
2013-01-15 11:40:50 +1000

    Update deploy scripts

--- scripts/redbox-clean.sh
+++ scripts/redbox-clean.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 SCRIPT_DIR=$(cd  $(dirname $0) ; pwd -P)
 source $SCRIPT_DIR/redbox-setup.sh
-exit 1
+
 echo "Clean up old deployment... waiting for 5 seconds"
 for count in 5 4 3 2 1
 do
--- scripts/redbox-settings.sh
+++ scripts/redbox-settings.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-PROJECT_HOME=""
+export PROJECT_HOME=""
 echo "You need to configure the variables in ~/.tdh-deploy-settings/redbox-settings.sh"; exit 1
 #comment out this ^ line when you have configured the variables
