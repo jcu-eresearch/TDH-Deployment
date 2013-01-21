e93c7c84907c29d91d23e4cbe2224a9dd8b9cd4c
NigelB <nigel.blair@gmail.com>
2013-01-21 11:32:04 +1000

    Update deploy scripts

--- scripts/mint-clean.sh
+++ scripts/mint-clean.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR=$(cd  $(dirname $0) ; pwd -P)
 source $SCRIPT_DIR/mint-setup.sh
 
-if [ ! -z  $PROJECT_DIR ]
+if [ ! -z  $PROJECT_DIR ] && [ -e  $PROJECT_DIR ]
 then
     rm -Rf $PROJECT_DIR
 fi
--- scripts/mint-shutdown.sh
+++ scripts/mint-shutdown.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR=$(cd  $(dirname $0) ; pwd -P)
 source $SCRIPT_DIR/mint-setup.sh
 
-if [ ! -z  $PROJECT_DIR  ]
+if [ ! -z  $PROJECT_DIR  ] && [ -e  $PROJECT_DIR ]
 then
     echo "Stopping Mint..."
     cat $INSTITUTIONAL_BUILD_DIR/.project-home
