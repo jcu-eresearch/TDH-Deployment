3c0eccfa135bb245a1a032104ae34cfc697e247b
nigelb <nigel.blair+github@gmail.com>
2013-02-21 12:08:25 +1000

    Change sh to bash

--- scripts/redbox-clean.sh
+++ scripts/redbox-clean.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 SCRIPT_DIR=$(cd  $(dirname $0) ; pwd -P)
 source $SCRIPT_DIR/redbox-setup.sh
--- scripts/redbox-deploy.sh
+++ scripts/redbox-deploy.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 SCRIPT_DIR=$(cd  $(dirname $0) ; pwd -P)
 export INSTITUTIONAL_BUILD_DIR=$1
